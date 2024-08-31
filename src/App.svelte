71aaa9731ad05588f72c794248e5be4bbc7886ee
Kresna Satya <kresnasatya72@gmail.com>
2024-08-31 11:58:35 +0800

    create example

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import sun from './assets/icons/sun.svg';
+  import moon from './assets/icons/moon.svg';
+  import Icon from './lib/Icon.svelte';
+</script>
+
+<p>Load svg by pass it to <code>src</code> attribute in <code>img</code> tag.</p>
+<img src={sun} alt="Sun" />
+<img src={moon} alt="Moon" />
+
+<p>Load svg by create Svelte component named <code>Icon.svelte</code></p>
+
+<Icon name="sun"/>
+<Icon name="moon"/>
+
+<p>We can pass additional attributes to update <code>fill</code> and <code>stroke</code> values in <code>svg</code> html.</p>
+
+<p>Day time: <Icon name="sun" time="morning"/> <Icon name="sun" time="afternoon" /></p>
+<p>Night time: <Icon name="moon" time="evening"/> <Icon name="moon" time="night" /></p>
---
+++ src/lib/Icon.svelte
@@ -0,0 +1,52 @@
+<script>
+    import MoonIcon from '@icons/moon.svg?raw';
+    import SunIcon from '@icons/sun.svg?raw';
+
+    const mapIcons = new Map();
+
+    mapIcons.set('sun', SunIcon);
+    mapIcons.set('moon', MoonIcon);
+
+    export let name = 'sun';
+    export let time = 'unknown';
+    export let width = '24';
+    export let height = '24';
+    export let fill = 'none';
+    export let stroke = 'currentColor';
+    export let strokeWidth = '2';
+
+    switch (time) {
+        case 'morning':
+            fill = '#FFF9C4'; /* Light Yellow */
+            stroke = '#333333'; /* Dark Gray */
+            break;
+        case 'afternoon':
+            fill = '#FFEB3B'; /* Bright Yellow */
+            stroke = '#212121'; /* Darker Gray */
+            break;
+        case 'evening':
+            fill = '#4b0082'; /* Indigo for evening */
+            stroke = '#dda0dd'; /* Purple for evening */
+            break;
+        case 'night':
+            fill = '#000033'; /* Very dark blue for night */
+            stroke = '#ffffff'; /* White for night text */
+            break;
+    }
+</script>
+
+<div role="img" aria-label="{time} time" style="display: inline; --width: {width}px; --height: {height}px; --stroke-width: {strokeWidth};
+    --stroke: {stroke}; --fill: {fill};">
+    {@html mapIcons.get(name)}
+</div>
+
+<style>
+    /* CSS modules */
+    div :global(svg) {
+        width: var(--width);
+        height: var(--height);
+        fill: var(--fill);
+        stroke-width: var(--stroke-width);
+        stroke: var(--stroke);
+    }
+</style>
